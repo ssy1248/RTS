4f4dfa6e011455627cf2eaea937715d5455d4007
Song Seung ryul <84385991+ssy1248@users.noreply.github.com>
2025-09-03 19:31:56 +0900

    feat: unit movement, unit select

---
+++ Assets/UnitMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour
+{
+    Camera cam;
+    NavMeshAgent agent;
+    public LayerMask ground;
+
+    public bool isCommandedToMove;
+
+    //DirectionIndicator directionIndicator;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+
+        //directionIndicator = GetComponent<DirectionIndicator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                isCommandedToMove = true;
+                agent.SetDestination(hit.point);
+
+                //directionIndicator.DrawLine(hit);
+            }
+        }
+
+        // Agent reached destination
+        if (agent.hasPath == false || agent.remainingDistance <= agent.stoppingDistance)
+        {
+            isCommandedToMove = false;
+        }
+    }
+}
